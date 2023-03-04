b253bf0b5f4af0878d7325b4993264bd6dfa7717
aayan malhotra <aayanmalhotra0001@gmail.com>
2023-03-04 14:15:36 +0530

    add changes

---
+++ release.config.js
@@ -0,0 +1,6 @@
+module.exports ={
+    branches: ["master"],
+    repositoryUrl: "https://github.com/1OMKAR7NAM/react-app",
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
+
+}
