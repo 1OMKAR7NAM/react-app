f67ce42e8b66178f00c24bcfe5e33ceb19feca1d
aayan malhotra <aayanmalhotra0001@gmail.com>
2023-03-04 14:17:19 +0530

    fix: fix description

--- release.config.js
+++ release.config.js
@@ -2,5 +2,4 @@ module.exports ={
     branches: ["master"],
     repositoryUrl: "https://github.com/1OMKAR7NAM/react-app",
     plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
-
 }
